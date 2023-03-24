5dcf34dc960ddc25459bec313493c7bb7d0b4589
tongyang <tongyang@diligrp.com>
2023-03-24 18:21:14 +0800

    feat: 补充手写代码题库

--- folder_js/01.js
+++ folder_js/01.js
@@ -3,7 +3,7 @@
  * tip:将传入的对象作为原型
  */
 function create(obj) {
-  function F() {};
+  function F() {}
   F.prototype = obj;
   return F();
 }
@@ -16,14 +16,14 @@ function create(obj) {
  */
 function myInstanceof(left, right) {
   let proto = Object.getPrototypeOf(left),
-      prototype = right.prototype;
+    prototype = right.prototype;
 
-      // 判断构造函数的prototype对象是否在对象的原型链上
-      while(true) {
-        if (!proto) return false;
-        if (proto === prototype) return true;
-        proto = Object.getPrototypeOf(proto);
-      }
+  // 判断构造函数的prototype对象是否在对象的原型链上
+  while (true) {
+    if (!proto) return false;
+    if (proto === prototype) return true;
+    proto = Object.getPrototypeOf(proto);
+  }
 }
 
 /**
@@ -35,8 +35,8 @@ function myInstanceof(left, right) {
  */
 function myNew(fn, ...args) {
   // 判断参数是不是函数
-  if (typeof fn !== 'function') {
-    return console.error('type error!');
+  if (typeof fn !== "function") {
+    return console.error("type error!");
   }
   // 创建一个对象，将该对象的原型设置为构造函数的prototype对象
   let obj = Object.create(fn.prototype);
@@ -54,15 +54,15 @@ function myNew(fn, ...args) {
  *      可用于scroll函数的事件监听上，通过节流降低事件调用的频率
  */
 function debounce(fn, delay) {
-  let timer = null
-  return function(...args) {
+  let timer = null;
+  return function (...args) {
     if (timer) {
-      clearTimeout(timer)
+      clearTimeout(timer);
     }
     timer = setTimeout(() => {
-      fn.apply(this, args)
+      fn.apply(this, args);
     }, delay);
-  }
+  };
 }
 
 function throttle(fn, delay) {
@@ -70,7 +70,7 @@ function throttle(fn, delay) {
   // 转换失败的话返回NaN，+new Date() 会调用Date.prototype上面的valueOf方法，隐式转换
   // new Date().getTime() === new Date().valueOf() === +new Date() === new Date()*1 === Date.now()/H5新增
   let time = +new Date();
-  return function(...args) {
+  return function (...args) {
     const now = +new Date();
     if (now - time >= delay) {
       setTimeout(() => {
@@ -78,7 +78,7 @@ function throttle(fn, delay) {
       }, delay);
       time = now;
     }
-  }
+  };
 }
 
 /**
@@ -93,12 +93,12 @@ function throttle(fn, delay) {
  */
 Function.prototype.myCall = function (context) {
   // 判断调用对象
-  if (typeof this !== 'function') {
-    throw new Error('type error');
+  if (typeof this !== "function") {
+    throw new Error("type error");
   }
   // 处理参数
   let params = [...arguments].slice(1),
-      result = null;
+    result = null;
   // 判断context是否传入，如果没有，则赋值window
   context = context || window;
   // 将调用函数作为context的方法
@@ -108,10 +108,10 @@ Function.prototype.myCall = function (context) {
   // 将属性删除
   delete context.fn;
   return result;
-}
+};
 Function.prototype.myApply = function (context) {
-  if (typeof this !== 'function') {
-    throw new Error('type error');
+  if (typeof this !== "function") {
+    throw new Error("type error");
   }
   let result = null;
   context = context || window;
@@ -123,14 +123,14 @@ Function.prototype.myApply = function (context) {
   }
   delete context.fn;
   return result;
-}
+};
 Function.prototype.myBind = function (context) {
-  if (typeof this !== 'function') {
-    throw new Error('type error')
+  if (typeof this !== "function") {
+    throw new Error("type error");
   }
   // 处理参数
   let params = [...arguments].slice(1),
-      fn = this;
+    fn = this;
   context = context || window;
   return function Fn() {
     // 根据调用方式，传入不同绑定值
@@ -141,9 +141,9 @@ Function.prototype.myBind = function (context) {
       // 因为优先级 new绑定 > 显示绑定
       this instanceof Fn ? this : context,
       params.concat(...arguments)
-    )
-  }
-}
+    );
+  };
+};
 
 /**
  * 6、函数柯里化
@@ -157,10 +157,10 @@ function myCurry(fn) {
       fn.call(null, ...args1);
     } else {
       return function (...args2) {
-        return curry.apply(null, [...args1, ...args2])
-      }
+        return curry.apply(null, [...args1, ...args2]);
+      };
     }
-  }
+  };
 }
 function curry(fn, ...args) {
   return args.length >= fn.length ? fn(...args) : fn.bind(null, fn, ...args);
@@ -176,7 +176,7 @@ function curry(fn, ...args) {
  * 4）当对象的属性和监听函数设置完成后，最后通过调用sent方法来向服务器发送请求，可以传入参数作为发送的数据体
  */
 function myAjax() {
-  const url = '/server';
+  const url = "/server";
   let xhr = new XMLHttpRequest();
   xhr.open("GET", url, true);
   xhr.onreadystatechange = function () {
@@ -187,12 +187,12 @@ function myAjax() {
     } else {
       console.error(this.statusText);
     }
-  }
+  };
   xhr.onerror = function () {
     console.error(this.statusText);
-  }
-  xhr.responseType = 'json';
-  xhr.setRequestHeader('Accept', 'application/json');
+  };
+  xhr.responseType = "json";
+  xhr.setRequestHeader("Accept", "application/json");
   xhr.send(null);
 }
 
@@ -201,7 +201,7 @@ function myAjax() {
  */
 function getJson() {
   return new Promise((resolve, reject) => {
-    const url = '/server';
+    const url = "/server";
     let xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function () {
@@ -209,20 +209,20 @@ function getJson() {
       if (this.status === 200 || this.status === 304) {
         // request success callback
         console.log(this.response);
-        resolve(this.response)
+        resolve(this.response);
       } else {
         console.error(this.statusText);
-        reject(new Error(this.statusText))
+        reject(new Error(this.statusText));
       }
-    }
+    };
     xhr.onerror = function () {
       console.error(this.statusText);
-      reject(new Error(this.statusText))
-    }
-    xhr.responseType = 'json';
-    xhr.setRequestHeader('Accept', 'application/json');
+      reject(new Error(this.statusText));
+    };
+    xhr.responseType = "json";
+    xhr.setRequestHeader("Accept", "application/json");
     xhr.send(null);
-  })
+  });
 }
 
 /**
@@ -230,13 +230,13 @@ function getJson() {
  */
 function deepClone(obj) {
   const type = Object.prototype.toString.call(obj).slice(8, -1);
-  if (type === 'Object') {
+  if (type === "Object") {
     let result = {};
     for (const key in obj) {
-      result[key] = deepClone(obj[key])
+      result[key] = deepClone(obj[key]);
     }
-    return result
-  } else if (type === 'Array') {
+    return result;
+  } else if (type === "Array") {
     let result = [];
     for (let i = 0; i < obj.length; i++) {
       result.push(deepClone(obj[i]));
@@ -251,7 +251,7 @@ function deepClone(obj) {
  * 1）遍历元素，将当前元素的当前索引以及之后的索引随机生成一个索引值，两两交换，以此类推直至结束
  */
 function randomIndexFn() {
-  let arr = [1,2,3,4,5,6,7,8,9,10];
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   for (let i = 0; i < arr.length; i++) {
     const randomIndex = Math.round(Math.random() * (arr.length - 1 - i)) + i;
     [arr[i], arr[randomIndex]] = [arr[randomIndex], arr[i]];
@@ -270,19 +270,17 @@ function flatArray1(arr) {
   let result = [];
   for (let i = 0; i < arr.length; i++) {
     if (Array.isArray(arr[i])) {
-      result = result.concat(flatArray1(arr[i]))
+      result = result.concat(flatArray1(arr[i]));
     } else {
-      result.push(arr[i])
+      result.push(arr[i]);
     }
   }
   return result;
 }
 function flatArray2(arr) {
   return arr.reduce((prev, next) => {
-    return prev.concat(
-      Array.isArray(next) ? flatArray2(next) : next
-    )
-  }, [])
+    return prev.concat(Array.isArray(next) ? flatArray2(next) : next);
+  }, []);
 }
 function flatArray3(arr) {
   return arr.flat(Infinity);
@@ -296,10 +294,8 @@ function _flat(arr, depth) {
     return arr;
   }
   return arr.reduce((prev, next) => {
-    return prev.concat(
-      Array.isArray(next) ? _flat(next, depth - 1) : next
-    )
-  }, [])
+    return prev.concat(Array.isArray(next) ? _flat(next, depth - 1) : next);
+  }, []);
 }
 // const arrNum = [1, [2, 3], [4, [5, 6, 7]]]
 // console.log(_flat(arrNum, 1))
@@ -354,7 +350,7 @@ function addFn() {
     return add;
   }
   // 创建一个取代valueOf方法的函数，覆盖自定义对象的valueOf方法，用于隐式转换
-  add.toString = () => result.reduce((prev, next) => prev + next, 0)
+  add.toString = () => result.reduce((prev, next) => prev + next, 0);
   return add;
 }
 // let add = addFn();
@@ -364,12 +360,14 @@ function addFn() {
 function addCurrying(fn, length) {
   // 第一次调用，给length赋值fn的长度，后面每次重复调用，length的长度都会减去参数的长度
   length = length || fn.length;
-  return function(...args) {
-    return args.length >= length ? fn.apply(this, args) : currying(fn.bind(this, ...args), length - args.length)
-  }
+  return function (...args) {
+    return args.length >= length
+      ? fn.apply(this, args)
+      : currying(fn.bind(this, ...args), length - args.length);
+  };
 }
 function fn(a, b, c, d) {
-  return a + b + c + d
+  return a + b + c + d;
 }
 // let add1 = addCurrying(fn);
 // console.log(add1(1)(2)(3)(4))
@@ -382,52 +380,200 @@ let imageAsync = (url) => {
     let img = new Image();
     img.src = url;
     img.onload = () => {
-      console.log('图片加载成功');
+      console.log("图片加载成功");
       resolve(img);
-    }
+    };
     img.onerror = (err) => {
-      console.error('图片加载失败');
+      console.error("图片加载失败");
       reject(err);
-    }
-  })
-}
-imageAsync('').then(
-  res => console.log('res', res),
-  err => console.log('err', err),
-).catch((error) => console.error(error))
+    };
+  });
+};
+imageAsync("")
+  .then(
+    (res) => console.log("res", res),
+    (err) => console.log("err", err)
+  )
+  .catch((error) => console.error(error));
 
 /**
  * 18、手写发布订阅模式
  */
 class EventCenter {
   constructor() {
-    this.handlers = {}
+    this.handlers = {};
   }
   addEventListener(type, handler) {
     if (!this.handlers[type]) {
       this.handlers[type] = [];
     }
-    this.handlers[type].push(handler)
+    this.handlers[type].push(handler);
   }
   dispatchEvent(type, params) {
     if (!this.handlers[type]) {
-      return new Error('该事件未注册')
+      return new Error("该事件未注册");
     }
-    this.handlers[type].forEach(handler => {
-      handler(...params)
+    this.handlers[type].forEach((handler) => {
+      handler(...params);
     });
   }
   removeEventListener(type, handler) {
     if (!this.handlers[type]) {
-      return new Error('事件无效')
+      return new Error("事件无效");
     }
-    const index = this.handlers[type].findIndex(e => e === handler);
+    const index = this.handlers[type].findIndex((e) => e === handler);
     if (index < 0) {
-      return new Error('无该绑定事件')
+      return new Error("无该绑定事件");
     }
     this.handlers[type].splice(index, 1);
     if (this.handlers[type].length === 0) {
-      delete this.handlers[type]
+      delete this.handlers[type];
     }
   }
+}
+
+/**
+ * 19、Vue2-object.defineProperty(简易版)
+ * 1）Vue2响应式原理：结合了object.defineProperty的数据劫持，以及发布订阅模式
+ * 2）Vue2的数据劫持，通过递归遍历data里的数据，用object.defineProperty给每一个属性添加getter和setter，
+ *    并且把data里的属性挂载到vue实例中，修改vue实例上的属性时，会触发对应的setter函数，向Dep订阅器发布更新消息，
+ *    对应的Watcher订阅者会收到通知，调用自身的回调函数，让编译器去更新视图。
+ */
+{
+  const obj = {
+    name: "zhangsan",
+    age: 26,
+  };
+  const p = {};
+  for (const key in obj) {
+    Object.defineProperty(p, key, {
+      get() {
+        console.log(`有人读取p里的${key}属性`);
+        return obj[key];
+      },
+      set(val) {
+        console.log(`有人修改了p里的${key}属性,值为${val},需要去更新视图`);
+        obj[key] = val;
+      },
+    });
+  }
+}
+
+/**
+ * 20、Vue3-Proxy数据劫持(简易版)
+ * 1）vue3的数据劫持通过Proxy函数对代理对象的属性进行劫持，通过Reflect对象里的方法对代理对象的属性进行修改
+ * 2）Proxy代理对象不需要遍历，配置项里的回调函数可以通过参数拿到修改属性的键和值
+ * 3）这里用到了Reflect对象里的三个方法，get、set、deleteProperty，方法需要的参数与配置项中回调函数的参数相同
+ * 4）Reflect里的方法和Proxy里的方法是一一对应的，只要是Proxy对象的方法就能在Reflect对象上找到对应的方法。
+ */
+{
+  const obj = {
+    name: 'ttyy',
+    age: 26,
+  }
+  const p = new Proxy(obj, {
+    // 读取属性的时候调用, target为代理的源对象
+    get(target, propName) {
+      console.log(`有人读取p对象里的${propName}属性`);
+      return Reflect.get(target, propName);
+    },
+    // 添加和修改属性时调用
+    set(target, propName, value) {
+      console.log(`有人修改了p对象里的${propName}属性,值为${value},需要去修改视图`);
+      Reflect.set(target, propName, value);
+    },
+    // 删除属性时调用
+    deleteProperty(target, propName) {
+      console.log(`有人删除了p对象里的${propName}属性，需要去修改视图`);
+      return Reflect.deleteProperty(target, propName);
+    }
+  })
+}
+
+/**
+ * 21、实现路由(简易版)
+ */
+class Route {
+  constructor() {
+    // 路由存储对象
+    this.routes = {};
+    // 当前路由
+    this.currentHash = '';
+    // 绑定this，避免监听时改变this指向
+    this.freshRoute = this.freshRoute.bind(this);
+    // 监听
+    window.addEventListener('load', this.freshRoute, false);
+    window.addEventListener('hashchange', this.freshRoute, false);
+  }
+  // 存储
+  storeRoute (path, cb) {
+    this.routes[path] = cb || function () {};
+  }
+  // 更新
+  freshRoute () {
+    this.currentHash = window.location.hash.slice(1) || '/';
+    this.routes[this.currentHash]();
+  }
+}
+
+/**
+ * 22、setTimeout相互转换setInterval
+ * 思路：使用递归函数，不断的执行setTimeout实现setInterval的效果
+ */
+function mySetInterval (fn, time) {
+  // 控制器，控制定时器是否继续进行
+  var timer = {
+    flag: true
+  }
+  // 设置递归函数，模拟定时器执行。
+  function interval() {
+    if (timer.flag) {
+      fn();
+      setTimeout(interval, timeout);
+    }
+  }
+  // 启动定时器
+  setTimeout(interval, time);
+  // 返回控制器
+  return timer;
+}
+function mySetInterval (fn, time) {
+  const interval = setInterval(() => {
+    clearInterval(interval)
+    fn();
+  }, time);
+}
+
+/**
+ * 23、实现jsonp
+ */
+{
+  // 动态加载js文件
+  function addScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.type = 'text/javascript';
+    document.body.appendChild(script);
+  }
+  function handleRes (res) { // res = {a: 1, b: 2}
+    console.log('res', res);
+  }
+  addScript("http://xxx.xxx.com/xxx.js?callback=handleRes");
+}
+
+/**
+ * 24、提取出url的参数并转化为对象
+ * [^?&=]+ 匹配非?|&|=的非空字符串
+ * 'https://www.junjin.cn?a=1&b=2' arguments
+ * ['a=1', 'a', '1', 22, 'https://www.junjin.cn?a=1&b=2']
+ * ['b=2', 'b', '2', 26, 'https://www.junjin.cn?a=1&b=2']
+ * url.match(reg) => ['a=1', 'b=2']
+ */
+function getUrlParams(url) {
+  let reg = /([^?&=]+)=([^?&=]+)/g;
+  const obj = {};
+  url.replace(reg, function() {
+    obj[arguments[1]] = arguments[2]
+  })
+  return obj;
 }
